60b00a96de5098a2a1c4dce95ab83ea95dc634c3
Nadia Fallahtafti <nadia.ft.72@gmail.com>
2023-06-30 09:56:11 +0330

    added check_disk_full function.

--- all_checks.py
+++ all_checks.py
@@ -6,6 +6,15 @@ def check_reboot():
     "return True if the computer has a pending reboot"
     return os.path.exists("/run/reboot-required")
 
+def check_disk_full(disk, min_gb, min_percent):
+    "true = there is enough free disk space"
+    du = shutil.disk_usage(disk)
+    percent_free = 100 * du.free / du.total
+    gigabytes_free = du.free / 2**30 
+    if percent_free<min_percent or gigabytes_free < min_gb:
+        return True
+    return False
+
 
 def main():
     if check_reboot():
