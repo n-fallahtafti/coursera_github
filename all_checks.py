dfc2e4a7616cb8e818bb4ff46bc56126b5aa1c62
Nadia Fallahtafti <33514608+nadiaft72@users.noreply.github.com>
2023-06-30 09:53:52 +0330

    Update all_checks.py

--- all_checks.py
+++ all_checks.py
@@ -5,7 +5,9 @@ import shutil
 def check_reboot():
     "return True if the computer has a pending reboot"
     return os.path.exists("/run/reboot-required")
-
+print("other person is working :)) ")
+def check_disk_full(disk, min_gb, min_percent):
+    pass
 
 def main():
     if check_reboot():
