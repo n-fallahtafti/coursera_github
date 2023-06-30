ccb8fb3e437828464263f3a0cc571a44f3287e58
Nadia Fallahtafti <33514608+nadiaft72@users.noreply.github.com>
2023-06-30 09:07:32 +0330

    Update all_checks.py

--- all_checks.py
+++ all_checks.py
@@ -5,7 +5,7 @@ def check_reboot():
     "return True if the computer has a pending reboot"
     return os.path.exists("/run/reboot-required")
 
-
+print("other person is working :)) ")
 def main():
     if check_reboot():
         print("pending reboot.")
