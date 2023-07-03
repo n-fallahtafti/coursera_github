61e3d366eaa53d396cefa9e92284ee5fb0aa69a8
nadiaft72 <nadia.ft.72@gmail.com>
2023-07-03 08:39:44 +0330

    add a simple network connectivity check

--- all_checks.py
+++ all_checks.py
@@ -1,5 +1,8 @@
 import os
 import shutil
+import socket
+import psutil
+
 #these functions check the disk ! 
 def check_reboot():
     "return True if the computer has a pending reboot"
@@ -15,14 +18,25 @@ def check_disk_full(disk, min_gb, min_percent):
 def check_root_full():
     "True = root partition is full."
     return check_disk_full(disk="/", min_gb=2, min_percent=10)
+
 def check_cpu_constrained():
     """True = CPU is having too much usage"""
-    return psutil.cpu_persent(1) > 75
+    return psutil.cpu_percent(1) > 75
+
+def check_no_network():
+    """true = fails to resolve Google's URL """
+    try:
+        socket.gethostbyname("www.google.com")
+        return False
+    except:
+        return True
 
 def main():
     checks=[
         (check_reboot,"pending reboot"),
-        (check_root_full,"root is full.")
+        (check_root_full,"root is full."),
+        (check_cpu_constrained, "CPU load too high."),
+        (check_no_network,"no working network")
     ]
     everything_ok = True
     for check, msg in checks:
